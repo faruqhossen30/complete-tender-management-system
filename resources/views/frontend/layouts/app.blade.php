8b7c69112f7b6d993264ca6ba077c7a87bf34b0c
unknown <faruqhossen30@gmail.com>
2021-08-02 17:10:07 +0600

    fix css path using public

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <link rel="stylesheet" href="{{asset('css/cwlcustom.css')}}">
+    <link rel="stylesheet" href="{{asset('public/css/cwlcustom.css')}}">
 
     <title>Catalysts Wings Ltd | @yield('title', 'Catalysts Wings Ltd')</title>
   </head>
