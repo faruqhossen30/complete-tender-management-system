8f83f8704a565e3718474465a869af15d83f3e44
Sahadat Hossen <sahadatjh@gmail.com>
2021-08-09 11:12:20 +0600

    card footer static garbase data problem fix

--- resources/views/frontend/international.blade.php
+++ resources/views/frontend/international.blade.php
@@ -51,7 +51,7 @@
                                         </li>
                                         @if (isset($tender->tenderLocation->name))
                                         <li class="list-inline-item">
-                                            <a href="#" class="text-secondary"> <span class=""><i class="fa fa-compass"></i></span> |
+                                            <a href="#" class="text-secondary"> <span class=""><i class="fa fa-compass"></i></span> 
                                                 {{$tender->tenderLocation->name ?? "N/a"}} </a>
                                         </li>
                                         @endif
@@ -133,7 +133,7 @@
                                         </li>
                                         @if (isset($tender->tenderLocation->name))
                                         <li class="list-inline-item">
-                                            <a href="#" class="text-secondary"> <span class=""><i class="fa fa-compass"></i></span> |
+                                            <a href="#" class="text-secondary"> <span class=""><i class="fa fa-compass"></i></span> 
                                                 {{$tender->tenderLocation->name ?? "N/a"}} </a>
                                         </li>
                                         @endif
@@ -181,7 +181,7 @@
                                     </li>
                                     @if (isset($tender->tenderLocation->name))
                                     <li class="list-inline-item">
-                                        <a href="#" class="text-secondary"> <span class=""><i class="fa fa-compass"></i></span> |
+                                        <a href="#" class="text-secondary"> <span class=""><i class="fa fa-compass"></i></span> 
                                             {{$tender->tenderLocation->name ?? "N/a"}} </a>
                                     </li>
                                     @endif
