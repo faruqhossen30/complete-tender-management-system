b74058cb8c677eb83fb052f606cafa2952887d7e
Sahadat Hossen <sahadatjh@gmail.com>
2021-08-10 12:21:56 +0600

    tender applicatio showing page add field tender budget

--- resources/views/backend/admin/application/show.blade.php
+++ resources/views/backend/admin/application/show.blade.php
@@ -45,8 +45,8 @@
             <tr>
                 <th>Tender Id:</th>
                 <td>{{ $application->tenderInfo->tenderID }}</td>
-                <th>Last Date:</th>
-                <td>{{ $application->tenderInfo->lastDate}}</td>                
+                <th>Tender Budget:</th>
+                <td>{{ $application->tenderInfo->tenderBudget }}</td>
             </tr>
             <tr>
                 <th>Tender Description</th>
@@ -54,7 +54,6 @@
                 <th>Security:</th>
                 <td>{{ $application->tenderInfo->tenderSecurity }}</td>
             </tr>
-<<<<<<< HEAD
             <tr>
                 <th>Tender Category:</th>
                 <td>{{ $application->tenderInfo->tenderCategory->name }}</td>
@@ -67,15 +66,13 @@
                 <th>Location:</th>
                 <td>{{ $application->tenderInfo->tenderLocation->name }}</td>
             </tr>
-=======
-            {{-- <tr>
-                <th>Tender Id:</th>
-                <td>{{ $application->tenderInfo->tenderID }}</td>
-                <th>Tender Description</th>
-                <td>{{ $application->userInfo}}</td>
-            </tr> --}}
+            <tr>
+                <th>Last Date:</th>
+                <td>{{ $application->tenderInfo->lastDate}}</td>
+                <th></th>
+                <td></td>
+            </tr>
 
->>>>>>> master
         </table>
     </div>
 </div>
